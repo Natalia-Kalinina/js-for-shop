3d1c45eb4b3f40dd437ac90bf9c54125fd007d69
Natalia Kalinina <natalia.kalinina2907@gmail.com>
2020-04-10 22:51:03 +0300

    fake data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,23 @@
 let cartCounterLabel = document.querySelector('#cart-counter');
 let cartContainer = document.querySelector('.page-content');
+
 let cartCounter = 0;
+let cartPrice = 0;
 
 let btnClickHandler = (e) => {
     let target = e.target;
 
     if (target.classList.contains('item-actions__cart')) {
         cartCounterLabel.innerHTML = ++cartCounter;
+
+        let fakeData = +target.parentElement.
+        previousElementSibling.
+        innerHTML.
+        replace(/^\$(\d+)\s\D+(\d+).*$/gui, '$1.$2');
+
+        cartPrice = Math.round((cartPrice + fakeData) * 100) / 100;
+        console.log(cartPrice);
+
         if (cartCounter === 1) {
             cartCounterLabel.style.display = 'block'
         }
